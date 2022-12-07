8d19cac42cf98760a95e336e0de43fb705e91099
Samuel Barnes <sbarnes@vermeer.com>
2022-12-07 14:04:18 -0600

    day 5 part 1 wip

---
+++ AdventOfCode2022/Day5/Part1.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode2022.Day5;
+
+public class Part1 : BasePart
+{
+    public static string Run()
+    {
+        Start(5,1);
+        var input = LoadInput(5);
+        var stackTops = new List<char>();
+
+        var cratesInput = new List<string>();
+        var moveInput = new List<string>();
+        var crates = true;
+        input.ForEach(x =>
+        {
+            if (string.IsNullOrEmpty(x)) crates = false;
+            if(crates) cratesInput.Add(x);
+            else moveInput.Add(x);
+        });
+        
+        
+        return CompileStackTop(stackTops);
+    }
+
+    private static string CompileStackTop(List<char> stackTops)
+    {
+        var result = "";
+        stackTops.ForEach(x =>
+        {
+            result += x.ToString();
+        });
+        return result;
+    }
+}
+
+class CrateStack
+{
+    public CrateStack(List<char> stack)
+    {
+        Stack = stack;
+    }
+    public List<char> Stack { get; set; }
+}
+
+class StackSet
+{
+    public StackSet(List<List<char>> stacks)
+    {
+        Stacks = new List<CrateStack>();
+        stacks.ForEach(s =>
+        {
+            Stacks.Add(new CrateStack(s));
+        });
+    }
+    public List<CrateStack> Stacks { get; set; }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine(AdventOfCode2022.Day3.Part1.Run());
 Console.WriteLine(AdventOfCode2022.Day3.Part2.Run());
 Console.WriteLine(AdventOfCode2022.Day4.Part1.Run());
 Console.WriteLine(AdventOfCode2022.Day4.Part2.Run());
+Console.WriteLine(AdventOfCode2022.Day5.Part1.Run());
+// Console.WriteLine(AdventOfCode2022.Day5.Part2.Run());
