cc6db91e711da4e510a35918bb14b610904b1da7
Samuel Barnes <sbarnes@vermeer.com>
2022-12-07 17:15:04 -0600

    Day 7 Part 1 wip

---
+++ AdventOfCode2022/Day7/Part1.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2022.Day7;
+
+public class Part1 : BasePart
+{
+    public static int Run()
+    {
+        Start(7,1);
+        var input = LoadInput(7);
+
+        return 0;
+    }
+}
