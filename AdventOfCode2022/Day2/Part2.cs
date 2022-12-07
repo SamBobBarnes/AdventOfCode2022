280e0ed84af690f6a7adebf1a6673da11c8c63b6
Samuel Barnes <sbarnes@vermeer.com>
2022-12-07 12:15:31 -0600

    Day 2 Part 2

---
+++ AdventOfCode2022/Day2/Part2.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode2022.Day2;
+
+public class Part2 : BasePart
+{
+    public static int Run()
+    {
+        Start(2,2);
+        var input = System.IO.File.ReadAllText(@"C:\Users\sb17057\Repos\AdventOfCode\2022\AdventOfCode2022\AdventOfCode2022\Day2\Input.txt").Split("\r\n").ToList();
+
+        var score = 0;
+        input.ForEach(round =>
+        {
+            var moves = round.Split(" ");
+            score += ScoreRound(moves[0], moves[1]);
+        });
+        
+        return score;
+    }
+    
+    private static int ScoreRound(string them, string me)
+    {
+        var score = 0;
+
+        switch (me)
+        {
+            case "X": //Lose
+                score += 0;
+                switch (them)
+                {
+                    case "A": //Rock
+                        score += 3; 
+                        break;
+                    case "B": //Paper
+                        score += 1; 
+                        break;
+                    case "C": //Scissors
+                        score += 2; 
+                        break;
+                }
+                break;
+            case "Y": //Draw
+                score += 3;
+                switch (them)
+                {
+                    case "A": //Rock
+                        score += 1; 
+                        break;
+                    case "B": //Paper
+                        score += 2; 
+                        break;
+                    case "C": //Scissors
+                        score += 3; 
+                        break;
+                }
+                break;
+            case "Z": //Win
+                score += 6;
+                switch (them)
+                {
+                    case "A": //Rock
+                        score += 2; 
+                        break;
+                    case "B": //Paper
+                        score += 3; 
+                        break;
+                    case "C": //Scissors
+                        score += 1; 
+                        break;
+                }
+                break;
+        }
+        return score;
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AdventOfCode2022.Day1;
-using AdventOfCode2022.Day2;
-using Part1 = AdventOfCode2022.Day1.Part1;
-
-Console.WriteLine(Part1.Run());
-Console.WriteLine(Part2.Run());
+﻿Console.WriteLine(AdventOfCode2022.Day1.Part1.Run());
+Console.WriteLine(AdventOfCode2022.Day1.Part2.Run());
 Console.WriteLine(AdventOfCode2022.Day2.Part1.Run());
-//Console.WriteLine(Part2.Run());
+Console.WriteLine(AdventOfCode2022.Day2.Part2.Run());
