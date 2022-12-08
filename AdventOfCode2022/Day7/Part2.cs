44d2b14002b72da296d21c206ee35cdb180edf8a
SamBobBarnes <samuelrbarnes@outlook.com>
2022-12-07 21:25:10 -0600

    Day 7 Part 2

---
+++ AdventOfCode2022/Day7/Part2.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode2022.Day7;
+
+public class Part2 : BasePart
+{
+    public static int Run()
+    {
+        Start(7,2);
+        var input = LoadInput(7);
+        var commandList = input.Select(x => new Command(x)).ToList();
+        var currentFolder = new Folder("/");
+
+        commandList.ForEach(c =>
+        {
+            if(c.Type == CommandType.Cd)
+            {
+                if(c.Name == "..")
+                {
+                    currentFolder = currentFolder.GetParent();
+                }
+                else if(c.Name == "/")
+                {
+
+                }
+                else
+                {
+                    currentFolder = currentFolder.Goto(c.Name);
+                }
+            }
+            else if (c.Type == CommandType.File)
+            {
+                currentFolder.AddSubFile(c.Name, c.Size);
+            }
+        });
+
+
+        var topLevel = currentFolder.TopLevel();
+        var totalSpace = 70000000;
+        var spaceNeeded = 30000000;
+        var spaceUsed = topLevel.GetSize();
+        var spaceRemaining = totalSpace - spaceUsed;
+        var spaceToRemove = spaceNeeded - spaceRemaining;
+        Console.WriteLine(spaceUsed);
+        Console.WriteLine(spaceRemaining);
+        Console.WriteLine(spaceToRemove);
+        var smallestFolder = 30000000;
+        var folderSizes = topLevel.GetFolderSizes();
+
+        foreach (var item in folderSizes)
+        {
+            if (item.Value <= smallestFolder && item.Value > spaceToRemove) smallestFolder = item.Value;
+            Console.WriteLine($"Key = {item.Key}, Value = {item.Value}");
+        }
+        Console.WriteLine(spaceToRemove);
+        return smallestFolder;
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -10,5 +10,5 @@
 // Console.WriteLine(AdventOfCode2022.Day5.Part2.Run());
 // Console.WriteLine(AdventOfCode2022.Day6.Part1.Run());
 // Console.WriteLine(AdventOfCode2022.Day6.Part2.Run());
-Console.WriteLine(AdventOfCode2022.Day7.Part1.Run());
-// Console.WriteLine(AdventOfCode2022.Day7.Part2.Run());
+//Console.WriteLine(AdventOfCode2022.Day7.Part1.Run());
+Console.WriteLine(AdventOfCode2022.Day7.Part2.Run());
