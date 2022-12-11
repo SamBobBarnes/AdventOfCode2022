30319a2f1402688b1d7a9d1b230626d2e5039daf
Samuel Barnes <sbarnes@vermeer.com>
2022-12-11 15:22:57 -0600

    monkeys wip

--- AdventOfCode2022/Day11/Part2.cs
+++ AdventOfCode2022/Day11/Part2.cs
@@ -6,22 +6,15 @@ public class Part2 : BasePart
     {
         Start(11,1);
         var input = LoadInput(11);
-        // var bunch = new MonkeyBunch2(input);
-        // for (int i = 0; i < 20; i++)
-        // {
-        //     bunch.ExecuteRound();
-        // }
-        //
-        // Console.WriteLine(bunch.ToString());
+        var bunch = new MonkeyBunch2(input);
+        for (int i = 0; i < 20; i++)
+        { 
+            bunch.ExecuteRound();
+        }
         
-        //return bunch.GetMonkeyBusiness();
-
-        var hex1 = new Hexadecimal(675);
-        Console.WriteLine(hex1.ToString());
-        var hex2 = new Hexadecimal(26);
-        Console.WriteLine(hex2.ToString());
-        var hex3 = Hexadecimal.Modulo(hex1, hex2);
-        Console.WriteLine(hex3.ToString());
+        Console.WriteLine(bunch.ToString());
+        
+        return bunch.GetMonkeyBusiness();
         return 0;
     }
 }
