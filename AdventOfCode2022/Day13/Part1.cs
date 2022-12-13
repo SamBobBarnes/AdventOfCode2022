5f836880718582929f79e5b58d794859468a4ead
Samuel Barnes <sbarnes@vermeer.com>
2022-12-13 09:31:34 -0600

    day 13 setup

---
+++ AdventOfCode2022/Day13/Part1.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2022.Day13;
+
+public class Part1 : BasePart
+{
+    public static int Run()
+    {
+        Start(13,1);
+        var input = LoadInput(13);
+
+        return 0;
+    }
+}
