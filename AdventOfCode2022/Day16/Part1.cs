c44eb17c3da455bc43c38312a64e93207e29b271
SamBobBarnes <samuelrbarnes@outlook.com>
2022-12-17 14:26:36 -0600

    wip

--- AdventOfCode2022/Day16/Part1.cs
+++ AdventOfCode2022/Day16/Part1.cs
@@ -8,30 +8,68 @@ public class Part1 : BasePart
         var exampleInput = LoadInput(16, true);
         var input = LoadInput(16, false);
         var network = new TunnelNetwork(exampleInput);
+
+        var total = DFSBase(network);
+        
+        return total;
+    }
+
+    private static int DFSBase(TunnelNetwork network)
+    {
+        var total = 0;
+        var currentPosition = network.Valve("AA");
         var shortestPaths = network.ShortestPaths();
         var timeRemaining = 30;
-
-        var currentPosition = network.Valve("AA");
         var unOpenedValves = network.UnOpenedValves();
         var visited = new Dictionary<Valve, bool>();
         foreach (var valve in unOpenedValves)
         {
-            visited.Add(valve,false);
+            visited.Add(valve, false);
         }
 
-        var total = DFS(visited, currentPosition, shortestPaths, timeRemaining, 0);
-        
-        return 0;
-    }
-
-    private static int DFS(Dictionary<Valve,bool> visited, Valve current, Dictionary<Valve,Dictionary<Valve,int>> shortestPaths, int timeRemaining, int total)
-    {
-        
+        foreach(var valve in visited)
+        {
+            timeRemaining -= shortestPaths[currentPosition][valve.Key] + 1;
+            visited[valve.Key] = true;
+            var visitedList = visited.Select(i => $"{i.Key.ToString()}: {i.Value}").ToList();
+            var pathTotal = DFS(visited, valve.Key, shortestPaths, timeRemaining, 0);
+            if (pathTotal > total) total = pathTotal;
+            visited[valve.Key] = false;
+            timeRemaining += shortestPaths[currentPosition][valve.Key] + 1;
+        }
 
         return total;
     }
 
-    private static int FlowToAdd(Dictionary<Valve, bool> visited)
+    private static int DFS(Dictionary<Valve,bool> visited, Valve current, Dictionary<Valve,Dictionary<Valve,int>> shortestPaths, int timeRemaining, int total)
+    {
+        if (timeRemaining <= 0) return 0;
+        visited[current] = true;
+        if (visited.Where(i => i.Value == false).Count() == 0)
+        {
+            for(int i = timeRemaining; i >= 0; i--)
+            {
+                total += FlowToAdd(visited,1);
+            }
+            return total;
+        }
+        foreach (var valve in visited)
+        {
+            if (valve.Value) continue;
+            timeRemaining -= shortestPaths[current][valve.Key] + 1;
+            var pathTotal = FlowToAdd(visited, shortestPaths[current][valve.Key] + 1);
+            visited[valve.Key] = true;
+            var visitedList = visited.Select(i => $"{i.Key.ToString()}: {i.Value}").ToList();
+            pathTotal = DFS(visited, valve.Key, shortestPaths, timeRemaining, pathTotal);
+            if (pathTotal > total) total = pathTotal;
+            visited[valve.Key] = false;
+            timeRemaining += shortestPaths[current][valve.Key] + 1;
+        }
+
+        return total;
+    }
+
+    private static int FlowToAdd(Dictionary<Valve, bool> visited, int timeTaken)
     {
         var total = 0;
         foreach (var valve in visited)
@@ -39,6 +77,6 @@ public class Part1 : BasePart
             if (valve.Value) total += valve.Key.FlowRate;
         }
 
-        return total;
+        return total * timeTaken;
     }
 }
